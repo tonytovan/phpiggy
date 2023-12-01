186d556ebfab739ce12f50328c4bdb3c38c234e6
tonytovan <122599116+tonytovan@users.noreply.github.com>
2023-12-01 11:38:59 +0100

    env files

---
+++ cli.php
@@ -0,0 +1,22 @@
+<?php
+
+require __DIR__ . "/vendor/autoload.php";
+
+include __DIR__ . '/src/Framework/Database.php';
+
+use Framework\Database;
+use Dotenv\Dotenv;
+use App\Config\Paths;
+
+$dotenv = Dotenv::createImmutable(Paths::ROOT);
+$dotenv->load();
+
+$db = new Database($_ENV['DB_DRIVER'], [
+    'host' => $_ENV['DB_HOST'],
+    'port' => $_ENV['DB_PORT'],
+    'dbname' => $_ENV['DB_NAME']
+], $_ENV['DB_USER'], $_ENV['DB_PASS']);
+
+$sqlFile = file_get_contents("./database.sql");
+
+$db->query($sqlFile);
